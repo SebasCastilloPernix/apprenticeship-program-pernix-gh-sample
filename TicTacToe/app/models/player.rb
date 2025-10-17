33bba4f7f1e227420934c019d994eef80b8349bc
SebasCastilloPernix <sebastian_orozco@pernixlabs.com>
2025-10-17 11:56:41 -0600

    feat: implement the player functionality for initialize and his respectives unit tests

--- TicTacToe/app/models/player.rb
+++ TicTacToe/app/models/player.rb
@@ -1,2 +1,9 @@
 class Player < ApplicationRecord
+  attr_reader :symbol
+
+  def initialize_player(option)
+    raise ArgumentError, "The option must be 'X' or 'O', not '#{option}'" unless %w[X O].include?(option)
+
+    @symbol = option
+  end
 end
