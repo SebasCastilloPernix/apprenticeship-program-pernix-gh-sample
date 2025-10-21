364e337770b57cd24ac9a49dd3def69eed9ccbd1
SebasCastilloPernix <sebastian_orozco@pernixlabs.com>
2025-10-21 15:12:21 -0600

    add the funcionality to make and validate if a movement is valid, with his unit tests

--- TicTacToe/app/models/board.rb
+++ TicTacToe/app/models/board.rb
@@ -1,7 +1,20 @@
+class InvalidMovement < StandardError
+end
+
+class InvalidLocation < StandardError
+end
+
 class Board < ApplicationRecord
   attr_reader :board
 
   def initialize_board
     @board = Array.new(9, ' ')
+  end
+
+  def make_movement(movement, player)
+    raise InvalidLocation.new("Invalid location") if movement < 0 || movement > 8
+    raise InvalidMovement.new("Invalid movement, that cell is already marked") if @board[movement] != ' '
+    # Ensures that the player is 'O' or 'X'
+    @board[movement] = player.symbol
   end
 end
